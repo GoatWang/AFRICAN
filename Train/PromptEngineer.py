1cd7f9f44f3a70232b6a7d076026e40973c0b4d4
GoatWang <jeremy4555@yahoo.com.tw>
2023-06-18 15:39:35 +0100

    drop segment as feature

--- Train/PromptEngineer.py
+++ Train/PromptEngineer.py
@@ -1,13 +1,12 @@
 import random
 
 templates = [
-    "A video of an animal {action}. This action is a kind of {action_category} behavior, which is performed by the {segment}.",
+    "A video of an animal {action}. This action is a kind of {action_category} behavior.",
 ]
 
 def generate_prompt(data):
-    action, action_category, segment = data['action'], data['action_category'], data['segment']
-    return random.choice(templates).format(action=action, action_category=action_category, segment=segment)
-
+    action, action_category = data['action'], data['action_category']
+    return random.choice(templates).format(action=action, action_category=action_category)
 
 if __name__ == "__main__":
-    print(generate_prompt("running", "locomotion", "legs"))
+    print(generate_prompt({"action": "running", "action_category": "locomotion"}))
