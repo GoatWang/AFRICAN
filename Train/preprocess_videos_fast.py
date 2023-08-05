0e3cdb16b4661e3f367b6836a3dce289341694b3
GoatWang <jeremy4555@yahoo.com.tw>
2023-08-05 01:05:14 +0100

    first

--- Train/preprocess_videos_fast.py
+++ Train/preprocess_videos_fast.py
@@ -84,7 +84,7 @@ def inference_preaug_save(_config, dataset, dataloader, image_encoder, pretraine
                     if not os.path.exists(video_feat_fast_fp):
                         torch.save(video_feats_fast[b, v].clone(), video_feat_fast_fp)
                         if _config['save_debug_frames']:
-                            torch.save(video_tensors_fast[b, v], video_feat_fast_fp.replace(".pt", "_debug.pt"))
+                            torch.save(video_tensors_fast[b, v].clone(), video_feat_fast_fp.replace(".pt", "_debug.pt"))
 
 
 @ex.automain
