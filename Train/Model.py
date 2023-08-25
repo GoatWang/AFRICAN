24d474451cd0dfabe1ad0e215f515726c8d357cb
GoatWang <jeremy4555@yahoo.com.tw>
2023-08-25 23:54:41 +0100

    VLCTransformer

--- Train/Model.py
+++ Train/Model.py
@@ -452,7 +452,7 @@ class AfricanSlowfast(pl.LightningModule):
                 st, end = idx*self.image_encoder_batch_size, (idx+1)*self.image_encoder_batch_size
                 frames_feats[st:end] = self.image_encoder_ic(frames_tensor[st:end])
             frames_feats = frames_feats.reshape(B, F, self.transformer_width_ic)
-        frames_feats = self.forward_framxes_feats_ic(frames_feats)
+        frames_feats = self.forward_frames_feats_ic(frames_feats)
         return frames_feats
     
     def forward_frames_feats_ic(self, frames_feats):
