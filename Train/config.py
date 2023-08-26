4e1db4014cfddb1f04bf607552bee9b92d0bf3ac
GoatWang <jeremy4555@yahoo.com.tw>
2023-08-26 09:59:20 +0100

    VCICAFLs1at_t1

--- Train/config.py
+++ Train/config.py
@@ -62,14 +62,14 @@ def config():
     enable_image_clip = True
     ckpt_path_ic = os.path.abspath(os.path.join(base_dir, "weights", "ViT-L-14.pt"))
     transformer_width_ic = 768
-    transformer_layers_ic = 3
+    transformer_layers_ic = 6
     transformer_heads_ic = 12
 
     ## african
     enable_african = True
     ckpt_path_af = os.path.abspath(os.path.join(base_dir, "weights", "clip_nodecay_infoNCE_8_rand_augmix_000030_epoch30.ckpt"))
     transformer_width_af = 768
-    transformer_layers_af = 3
+    transformer_layers_af = 6
     transformer_heads_af = 12
 
     # Viusialize Attn Map
